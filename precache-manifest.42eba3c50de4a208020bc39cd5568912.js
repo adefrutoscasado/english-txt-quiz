f2ffc8cb577035abe8ed5fe289aba61bf09e065c
adefrutoscasado <adefrutoscasado@gmail.com>
2026-09-13 15:39:26 +0200

    Updates

---
+++ precache-manifest.42eba3c50de4a208020bc39cd5568912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1422c9dfc91e37a6479ce0d509ba2f9e",
+    "url": "/english-txt-quiz/index.html"
+  },
+  {
+    "revision": "a41bbf050ea60f96419d",
+    "url": "/english-txt-quiz/static/css/main.97ac3cf3.chunk.css"
+  },
+  {
+    "revision": "dc2bfeafa299286086d6",
+    "url": "/english-txt-quiz/static/js/2.9af0ba31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/english-txt-quiz/static/js/2.9af0ba31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41bbf050ea60f96419d",
+    "url": "/english-txt-quiz/static/js/main.d1bf5aa0.chunk.js"
+  },
+  {
+    "revision": "551c05d9aff42e8352bd",
+    "url": "/english-txt-quiz/static/js/runtime-main.38df66be.js"
+  }
+]);
