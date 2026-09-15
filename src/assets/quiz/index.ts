b4903d74246ad724c77f68d34fd5c5f0a6a45790
Alejandro de Frutos <adefrutoscasado@gmail.com>
2026-09-15 16:03:42 +0200

    Register 2026-09-11 quiz

--- src/assets/quiz/index.ts
+++ src/assets/quiz/index.ts
@@ -1,8 +1,8 @@
 import quiz20260910 from './2026-09-10.json'
+import quiz20260911 from './2026-09-11.json'
 import quiz20260912 from './2026-09-12.json'
 import quiz20260913 from './2026-09-13.json'
 import quiz20260914 from './2026-09-14.json'
-import quiz20260915 from './2026-09-15.json'
 
 export interface QuizItem {
   question: string
@@ -14,10 +14,10 @@ export interface QuizItem {
 
 const quiz: QuizItem[] = [
   ...quiz20260910,
+  ...quiz20260911,
   ...quiz20260912,
   ...quiz20260913,
   ...quiz20260914,
-  ...quiz20260915,
 ]
 
 export default quiz
